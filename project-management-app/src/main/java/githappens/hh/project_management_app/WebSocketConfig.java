49f3695e60a91fb787441d752d74a1b12a8811b5
mari-sku <mari.skurnik@gmail.com>
2026-04-07 17:39:48 +0300

    fix websocketconfig

--- project-management-app/src/main/java/githappens/hh/project_management_app/WebSocketConfig.java
+++ project-management-app/src/main/java/githappens/hh/project_management_app/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
-         registry.addEndpoint("/websocket").withSockJS();
+         registry.addEndpoint("/websocket");
     }
 }
 
