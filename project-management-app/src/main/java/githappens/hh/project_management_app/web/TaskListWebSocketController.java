8451f723bc15566bda305d6f32e7a9684288fe22
mari-sku <mari.skurnik@gmail.com>
2026-04-09 13:30:34 +0300

    add payload to websocket controller

--- project-management-app/src/main/java/githappens/hh/project_management_app/web/TaskListWebSocketController.java
+++ project-management-app/src/main/java/githappens/hh/project_management_app/web/TaskListWebSocketController.java
@@ -2,8 +2,11 @@ package githappens.hh.project_management_app.web;
 
 import java.util.List;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import githappens.hh.project_management_app.domain.TaskList;
 import githappens.hh.project_management_app.domain.TaskListRepository;
@@ -11,6 +14,7 @@ import githappens.hh.project_management_app.domain.TaskListRepository;
 @Controller
 public class TaskListWebSocketController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TaskListWebSocketController.class);
     private TaskListRepository taskListRepository;
 
     public TaskListWebSocketController(TaskListRepository taskListRepository) {
@@ -19,7 +23,10 @@ public class TaskListWebSocketController {
 
     @MessageMapping("/getTasklists")
     @SendTo("/topic/tasklists")
-    public List<TaskList> getTasklists(Long projectId) {
-        return taskListRepository.findByProject_ProjectId(projectId);
+    public List<TaskList> getTasklists(@Payload Long projectId) {
+        logger.info("Received projectId: {}", projectId);
+        List<TaskList> tasklists = taskListRepository.findByProject_ProjectId(projectId);
+        logger.info("Found {} tasklists for projectId: {}", tasklists.size(), projectId);
+        return tasklists;
     }
 }
